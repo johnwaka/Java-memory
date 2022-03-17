b6d19b3dab63e1c1067f56188e350a2a271e425f
John Wakahiu <nyawirar@gmail.com>
2022-03-17 08:10:06 +0300

    Create intro.java

---
+++ intro.java
@@ -0,0 +1,19 @@
+//Create a new Project in an IDE(Eclipse or Intellij) and then create a class named Main
+public class Main {
+
+	public static void main(String[] args) {
+    
+		//This is an introduction to Java Programming
+    //  println outputs to a new line
+    //In Eclipse you can just type sysout and press Ctrl+Space to autocomplete
+		
+		System.out.println("Hello, good people");
+		System.out.println("It is your guy again!!");
+		
+		/*A variable is a placeholder for a value that behaves
+		as the value it contains. E.g x=123*/
+		
+	}
+
+
+}
