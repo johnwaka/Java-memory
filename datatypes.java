7a955637be3c4d8e111861cf835da6ea84694ca7
John Wakahiu <nyawirar@gmail.com>
2022-03-17 08:28:46 +0300

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,26 @@
+Data Type   SIZE                  VALUE
+Boolean     1 bit     primitive   True or False
+int         4 bytes   primitive   -2billion to 2 billion
+double      8 bytes   primitive   fractional number up to 15 digits eg. 3.(up to 15 decimal places)
+char        2 bytes   primitive   single character/letter/ASCII value eg 'h'
+String      varies    reference   a sequence of characters, eg "hello you"
+  
+  /*Primitive - predefined data types
+  8 types(byte, short, int, long, float, double, char and boolean)
+  Stores data
+  Can only hold 1 value
+  less memory
+  faster
+  
+  
+  */
+  /* Reference - unlimited and user defined eg Strings
+  Stores an address
+  Could hold more than 1 value
+  more memory
+  slow
+  */
+  
+  int x;        declaration
+  x = 123;      assignment
+  int x = 123;  initialization
